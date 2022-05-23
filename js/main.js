99854b73908dc16480c516998acae41356bd60dd
이민기 <psjlmk@gmail.com>
2022-05-23 21:24:51 +0900

    add - 코멘트 삭제 수정, 프로필 삭제수정, 모달 리로드, 메인 화면 사진 전환 fix - 기타 버그 수정

--- js/main.js
+++ js/main.js
@@ -1,59 +1,7 @@
 const backend_base_url = "http://127.0.0.1:4000";
 const frontend_base_url = "http://127.0.0.1:5500";
 
-async function Send() {
-  // async: 비동기 데이터 주고받기가 완료될때까지 기다린 후 다음 라인을 실행
-
-  const comment_data = {
-    text: document.getElementById("comment_text").value,
-  };
-
-  const response = await fetch(`${backend_base_url}/comment`, {
-    method: "POST",
-    body: JSON.stringify(comment_data),
-  });
-
-  response_json = await response.json();
-
-  if (response.status == 200) {
-    let comment_text = document.getElementById("comment_text");
-    let name = response_json.result;
-    comment_text.value = null;
-    console.log(name);
-  } else {
-    alert(response.status);
-  }
-}
-
-function enter_submmit() {
-  if (window.event.keyCode == 13) {
-    let text = document.getElementById("comment_text");
-    console.log(text);
-    comment();
-    document.getElementById("comment_text").value = "";
-  }
-}
-
-async function user_load() {
-  const response = await fetch(`${backend_base_url}/user_load`, {
-    method: "GET",
-  });
-
-  response_json = await response.json();
-  const users = response_json.users;
-  users.forEach((user) => {
-    html = `<div onclick="modal('${user._id}')">
-          <img src="${user.pr_photo}">
-      </div>`;
-
-    $("#member_list").append(html);
-  });
-
-  return response_json.users;
-}
-
 async function modal(idnumber) {
-    console.log(idnumber)
   const userdata = {
     idnumber: idnumber,
   };
@@ -65,15 +13,44 @@ async function modal(idnumber) {
 
   response_json = await response.json();
   const datas = response_json.datas;
+  const comments = response_json.comments;
 
   if (response.status == 200) {
     let _id = datas._id;
+    sessionStorage.setItem("_id", _id);
     let email = datas.email;
     let nick = datas.nick;
     let pr_photo = datas.pr_photo;
     let pr_desc = datas.pr_desc;
 
     let html = `<div class="modal_box">
+    <!-- 수정 팝업 -->
+    <div id="comment_edit_box" class="comment_edit_box">
+      <div class="comment_edit_title">
+        <div onclick="comment_edit_close()">
+         취소
+      </div>
+    <div>
+        댓글 수정
+    </div>
+      <div onclick="comment_edit_submit()">
+        수정
+      </div>
+    </div>
+    <div class="comment_edit_input">
+        <textarea id="edit_input"></textarea>
+    </div>
+    </div>
+    <!-- 수정 팝업끝 -->
+
+    <!-- 삭제 팝업 -->
+            <div class="comment_delete">
+                <button onclick="comment_delete_submit()">삭제</button>
+                <button>취소</button>
+            </div>
+    <!-- 삭제 팝업끝 -->
+
+
     <!-- 프로필 편집 -->
     <div id="pr_edit_box" class="pr_edit_box">
         <div class="pr_edit_title">
@@ -88,7 +65,7 @@ async function modal(idnumber) {
             </div>
         </div>
         <div class="pr_edit_input">
-            <textarea placeholder="Sports TMI....."></textarea>
+            <textarea id="pr_edit_input_text" placeholder="Sports TMI....."></textarea>
         </div>
     </div>
     <!-- 프로필 편집 END -->
@@ -102,7 +79,7 @@ async function modal(idnumber) {
         <div class="pr_desc">
             <div>
                 <div class="pr_desc_title">닉네임</div>
-                <div class="pr_desc_content">${nick}</div>
+                <div id="pr_desc_content" class="pr_desc_content">${nick}</div>
                 <div onclick=pr_edit() class="pr_desc_edit">프로필편집</div>
             </div>
             <div>
@@ -152,71 +129,124 @@ async function modal(idnumber) {
     </div>
     <!-- 댓글 입력창 끝 -->
 
-</div>`
+</div>`;
 
-    let comment1 = `<!-- 모달 코멘트 시작 -->
-    <div class="modal_comment">
-        <div class="comment_pr_photo"><img src="${pr_photo}"></div>
-        <div class="comment_content_box">
-            <div class="comment_content">
-                안치홍을 상대로는 볼넷을 내주며 3회 이후 처음으로 누상에 주자를 내보냈다. 팔의 힘이 빠진 듯 했다.
-            </div>
-            <div class="comment_bottom_info">
-                <div class="comment_nick">라면먹고싶다</div>
-                <div class="comment_content_time">방금</div>
-                <div class="comment_content_edit">수정</div>
-                <div class="comment_content_delete">삭제</div>
-            </div>
-        </div>
-    </div>
-    <!-- 모달 코멘트 끝 -->`
+    document.querySelector("#modal_body").innerHTML = html;
 
-    let comment2 = `<!-- 모달 코멘트 시작 -->
-    <div class="modal_comment">
-        <div class="comment_pr_photo"><img src="${pr_photo}"></div>
-        <div class="comment_content_box">
-            <div class="comment_content">
-                안녕하세요 요즘 들어 자주 들러요 행복한 하루 보내세요!
-            </div>
-            <div class="comment_bottom_info">
-                <div class="comment_nick">김밥먹고싶다</div>
-                <div class="comment_content_time">1초전</div>
-                <div class="comment_content_edit">수정</div>
-                <div class="comment_content_delete">삭제</div>
-            </div>
-        </div>
-    </div>
-    <!-- 모달 코멘트 끝 -->`
-
-
-
-    $('#modal_body').html(html)
-    $('#comment_list').append(comment1)
-    $('#comment_list').append(comment2)
+    comments.forEach((comment) => {
+      let comment_content = `<div class="modal_comment">
+      <div class="comment_pr_photo"><img src="2.jpeg"></div>
+      <div class="comment_content_box">
+          <div class="comment_content">
+              ${comment.comment}
+          </div>
+          <div class="comment_bottom_info">
+              <div class="comment_nick">${comment.nick}</div>
+              <div class="comment_content_time">${comment.date}</div>
+              <div onclick="comment_edit('${comment._id}')" class="comment_content_edit">수정</div>
+              <div onclick="comment_delete('${comment._id}')" class="comment_content_delete">삭제</div>
+          </div>
+      </div>
+  </div>`;
+      document.querySelector("#comment_list").innerHTML += comment_content;
+    });
 
     let modal = document.getElementById("modal_body");
     modal.style.display = "flex";
 
-    $("body")
-      .addClass("hidden")
-      .on("scroll touchmove mousewheel", function (e) {
-        e.preventDefault();
-      });
+    document.querySelector("body").classList.add("hidden");
 
-    $(document).mouseup(function (e) {
-      if ($(".modal_body").has(e.target).length === 0) {
-        $(".modal_body").css("display", "none");
-        $("body").removeClass("hidden").off("scroll touchmove mousewheel");
-      }
-    });
+    // window.onscroll = function () { window.scrollTo(0, 0); };
 
+    // $("body")
+    //   .addClass("hidden")
+    //   .on("scroll touchmove mousewheel", function (e) {
+    //     e.preventDefault();
+    //   });
 
-    
+    // $(document).mouseup(function (e) {
+    //   if ($(".modal_body").has(e.target).length === 0) {
+    //     $(".modal_body").css("display", "none");
+    //     $("body").removeClass("hidden").off("scroll touchmove mousewheel");
+    //   }
+    // });
+
+    document.querySelector("#comment_list").scrollTop =
+      document.querySelector("#comment_list").scrollHeight;
+  } else {
+    alert(response.status);
+  }
+}
+
+async function comment() {
+  // async: 비동기 데이터 주고받기가 완료될때까지 기다린 후 다음 라인을 실행
+
+  const comment_data = {
+    text: document.getElementById("comment_text").value,
+    nick: document.getElementById("pr_desc_content").innerText,
+  };
+
+  const response = await fetch(`${backend_base_url}/comment`, {
+    method: "POST",
+    body: JSON.stringify(comment_data),
+  });
+
+  response_json = await response.json();
+  let nickname = response_json.nickname;
+  let pr_photo = response_json.pr_photo;
+
+  if (response.status == 200) {
+    let comment_text = document.getElementById("comment_text");
+
+    html = `<div class="modal_comment">
+        <div class="comment_pr_photo"><img src="${pr_photo}"></div>
+        <div class="comment_content_box">
+            <div class="comment_content">
+                ${comment_text.value}
+            </div>
+            <div class="comment_bottom_info">
+            <div class="comment_nick">${nickname}</div>
+            <div class="comment_content_time">방금</div>
+            <div class="comment_content_edit">수정</div>
+            <div class="comment_content_delete">삭제</div>
+            </div>
+        </div>
+    </div>`;
+
+    feed_number = sessionStorage.getItem('_id')
+    modal(feed_number)
+    document.querySelector("#comment_list").scrollTop =
+      document.querySelector("#comment_list").scrollHeight;
   } else {
     alert(response.status);
   }
 
-   
+  return nickname;
+}
+
+function enter_submmit() {
+  if (window.event.keyCode == 13) {
+    let text = document.getElementById("comment_text");
+    comment();
+  }
+}
+
+async function user_load() {
+  const response = await fetch(`${backend_base_url}/user_load`, {
+    method: "GET",
+  });
+
+  response_json = await response.json();
+  const users = response_json.users;
+  users.forEach((user) => {
+    html = `<div onclick="modal('${user._id}')">
+          <img src="${user.pr_photo}">
+      </div>`;
+
+    document.querySelector("#member_list").innerHTML += html;
+  });
+
+  // return response_json.users;
 }
 
 function pr_edit() {
@@ -227,37 +257,151 @@ function pr_edit_close() {
   document.getElementById("pr_edit_box").style.display = "none";
 }
 
-function pr_edit_submit(email) {
-  console.log(email);
+function comment_edit_close() {
+  document.querySelector(".comment_edit_box").style.display = "none";
 }
 
-function comment() {
-  let text = document.getElementById("comment_text");
-  let comment_list = document.getElementById("comment_list");
-  let name = ["건빵먹고싶다", "핫도그먹고싶다", "햄버거먹고싶다"];
-  let pr_photo = ["1.jpeg", "2.jpeg", "3.jpeg"];
-  let namepick = Math.floor(Math.random() * name.length);
-  console.log(pr_photo[namepick]);
+async function pr_edit_submit(emailuid){
+  value = document.getElementById('pr_edit_input_text').value
+  const email = {
+    email: emailuid,
+    value : value,
+  };
 
-  Send();
+  const response = await fetch(`${backend_base_url}/pr_edit`, {
+    method: "POST",
+    body: JSON.stringify(email),
+  });
 
-  html = `<div class="modal_comment">
-        <div class="comment_pr_photo"><img src="./img/${pr_photo[namepick]}"></div>
-        <div class="comment_content_box">
-            <div class="comment_content">
-                ${text.value}
-            </div>
-            <div class="comment_bottom_info">
-            <div class="comment_nick">${name[namepick]}</div>
-            <div class="comment_content_time">방금</div>
-            <div class="comment_content_edit">수정</div>
-            <div class="comment_content_delete">삭제</div>
-            </div>
-        </div>
-    </div>`;
+  response_json = await response.json();
+  document.getElementById('pr_edit_box').style.display = "none"
+  feed_number = sessionStorage.getItem('_id')
+  modal(feed_number)
 
-  $("#comment_list").prepend(html);
-  text.value = null;
+  if (response.status == 200) {
+
+  }else{
+
+  }
+}
+
+async function comment_edit(cm_number) {
+  const commentdata = {
+    cm_number: cm_number,
+  };
+
+  const response = await fetch(`${backend_base_url}/comment_edit`, {
+    method: "POST",
+    body: JSON.stringify(commentdata),
+  });
+
+  // document.querySelector(".comment_edit_box").style.display = "block"
+
+  response_json = await response.json();
+
+  if (response.status == 200) {
+    const cm_data = response_json.cm_data;
+
+    document.querySelector("#edit_input").value = cm_data["comment"];
+    html = `        <div class="comment_edit_title">
+    <div onclick="comment_edit_close()">
+        취소
+    </div>
+    <div>
+        댓글 수정
+    </div>
+    <div onclick="comment_edit_submit('${cm_data._id}')">
+        수정
+    </div>
+</div>
+<div class="comment_edit_input">
+    <textarea id="edit_input">${cm_data["comment"]}</textarea>
+</div>`;
+    document.querySelector("#comment_edit_box").innerHTML = html;
+    document.querySelector("#comment_edit_box").style.display = "block";
+  } else {
+    alert(response.status);
+  }
+}
+
+async function comment_edit_submit(cm_number) {
+  value = document.querySelector("#edit_input").value;
+  const commentdata = {
+    cm_number: cm_number,
+    value: value,
+  };
+
+  const response = await fetch(`${backend_base_url}/comment_edit_submit`, {
+    method: "POST",
+    body: JSON.stringify(commentdata),
+  });
+
+  document.querySelector("#comment_edit_box").style.display = "none";
+
+  feed_number = sessionStorage.getItem('_id')
+  modal(feed_number)
+
+  response_json = await response.json();
+
+  if (response.status == 200) {
+  } else {
+    alert(response.status);
+  }
+}
+
+function comment_delete(cm_number) {
+  let number = cm_number
+  sessionStorage.setItem("cm_number", number);
+  document.querySelector('.comment_delete').style.display = "flex"
+}
+
+async function comment_delete_submit(){
+  cm_number = sessionStorage.getItem('cm_number')
+  document.querySelector('.comment_delete').style.display = "none"
+  
+  const data = {
+    cm_number: cm_number,
+  };
+
+  const response = await fetch(`${backend_base_url}/comment_delete`, {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+
+  response_json = await response.json();
+
+  if (response.status == 200) {
+    feed_number = sessionStorage.getItem('_id')
+    modal(feed_number)
+  } else {
+    alert(response.status);
+  }
+
+}
+
+function slide() {
+  img_list = [
+    "./img/baseball/2.jpg",
+    "./img/baseball/3.jpg",
+    "./img/baseball/4.jpg",
+    "./img/baseball/5.jpg",
+  ];
+
+  let count = 0;
+
+  let i = setInterval(function () {
+    // do your thing
+
+    html = `<img src="${img_list[count]}" id="slide" class="fadein">`;
+    document.getElementById("slide_main").innerHTML = html;
+    count++;
+    if (count === 4) {
+      count = 0;
+    }
+  }, 4000);
+
+  // setInterval(() => document.getElementById('slide').src = img_list[2], 2000);
 }
 
 user_load();
+slide();
